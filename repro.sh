f3c44a012b1c40ab265bb5020b4e18e37503e577
Edwin Török <edvin.torok@citrix.com>
2022-12-09 17:43:38 +0000

    add outputs

--- repro.sh
+++ repro.sh
@@ -1,2 +1,2 @@
 ulimit -c unlimited
-rm -f gen_lifecycle.bc gen_lifecycle.exe && dune clean && dune build && ./gen_lifecycle.exe; dune build --debug-artifact-substitution && ./gen_lifecycle.exe
+rm -f gen_lifecycle.bc gen_lifecycle.exe && dune clean && dune build && ./gen_lifecycle.exe; dune build --debug-artifact-substitution --verbose && ./gen_lifecycle.exe
